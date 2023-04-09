5586bf1ec44d02fea7e4236f99d72d0741b4f5a2
Popov Vladimir <tblkto2@rambler.ru>
2023-04-09 16:15:27 +0300

    corrected version with the right name

--- Navigation/Controllers/PostViewController.swift
+++ Navigation/Controllers/PostViewController.swift
@@ -8,42 +8,21 @@
 import UIKit
 
 class PostViewController: UIViewController {
-    
-    var somePost = Post(title: "My post!")
-    let buttonPost = UIButton()
-
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        configButton()
+        let barButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onTap))
+        navigationItem.rightBarButtonItem = barButton
+    }
 
-    }
-    
-    func configButton() {
-        view.addSubview(buttonPost)
-        buttonPost.translatesAutoresizingMaskIntoConstraints = false
-        buttonPost.backgroundColor = CustomColor().hexStringToUIColor(hex: "009FBD")
-        buttonPost.setTitle("Show modal screen", for: .normal)
-        buttonPost.titleLabel?.font = UIFont(name: "Show modal screen", size: 20)
-        buttonPost.layer.cornerRadius = 20
-        
-        NSLayoutConstraint.activate([
-            buttonPost.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
-            buttonPost.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -14),
-            buttonPost.widthAnchor.constraint(equalToConstant: 150),
-            buttonPost.heightAnchor.constraint(equalToConstant: 50)
-        ])
-        
-        buttonPost.addTarget(self, action: #selector(onTap), for: .touchUpInside)
-    }
-    
     func setupView() {
-        self.navigationItem.title = somePost.title
+        self.navigationItem.title = FeedViewController().somePost.title
         self.view.backgroundColor = CustomColor().hexStringToUIColor(hex: "C0DBEA")
     }
 
     @objc private func onTap() {
-        let someVC = ModalViewController()
+        let someVC = InfoViewController()
         let navVC = UINavigationController(rootViewController: someVC)
         present(navVC, animated: true)
     }
--- Navigation/Controllers/ProfileViewController.swift
+++ Navigation/Controllers/ProfileViewController.swift
@@ -14,36 +14,7 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = CustomColor().hexStringToUIColor(hex: "9ADCC9")
-        setupButton()
-    }
-    
-    private lazy var button: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = CustomColor().hexStringToUIColor(hex: "1A5F7A")
-        button.layer.cornerRadius = 20
-        button.setTitle("Ask question", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    @objc private func buttonAction() {
-        let alert = UIAlertController(title: "Вы точно отредактировали ТЗ к ДЗ?", message: "Это позитивно скажется на эффективности обучения!", preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "Да", style: .default, handler: { action in
-            print("Да, мы уже все попправили!")
-        }))
-        alert.addAction(UIAlertAction(title: "Нет", style: .cancel, handler: { action in print("И так сойдет") }))
-        
-        self.present(alert, animated: true)
-    }
-    
-    private func setupButton() {
-        self.view.addSubview(self.button)
-        self.button.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
-        self.button.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        self.button.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 14).isActive = true
-        self.button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
     }
 }
 
